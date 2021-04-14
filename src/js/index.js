f43524e82a3841b4e292154df1153c1cb6b6dad1
Majd Albahbahani <majd.albahbahani@gmail.com>
2021-04-14 14:47:56 +0300

    Kotobak first page

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,29 @@ import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'animate.css';
+
+// cuurent year
+new Date().getFullYear();
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// back to top button
+var mybutton = document.getElementById("backToTop");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+$("#backToTop").on("click", function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+  
+
